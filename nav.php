8acf728dd7c2252c0ef8b8ac975342d194d52ce5
justmeowww <kennethdoan12@gmail.com>
2024-09-30 05:03:43 -0500

    Javascript Update
    Added automation of galleries. Added image popup upon opening. Reorganized folders. Renamed photos.

---
+++ nav.php
@@ -0,0 +1,21 @@
+<div>
+    <img src="logos/andoPhotoLogo2.png" alt="Site Logo" width="70%"/>
+    <div class="break-line""></div>
+    <p>
+        This is where I’ll be sharing all my cosplay photography. 
+        I will try to make all photos available for viewing, but not at its full resolution. 
+        The full resolution download is only available to the clients.
+    </p>
+    <div class="break"></div>
+    <button class="nav-button-current" onclick="window.location.href = 'index.html'">
+        LATEST
+    </button>
+    <div class="break"></div>
+    <button class="nav-button" onclick="window.location.href = 'otherWorks.html'">
+        OTHER WORKS
+    </button>
+    <div class="break"></div>
+    <button class="nav-button" onclick="window.location.href = 'contact.html'">
+        CONTACT
+    </button>
+</div>
